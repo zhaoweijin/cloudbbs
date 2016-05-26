ac1f35c6b72038af52cfd0e7fdc33a9d397afdae
zhaoweijin <zhaoweijin@appgame.com>
2016-05-26 15:12:51 +0800

    Revert "no message"
    This reverts commit 07b3abe894766ace57fd4ebb0c83493bc720a659.

--- app/language/zh_cn/default_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang['front_home'] = "首页";
-
-
-/* End of file front_lang.php */
-/* Location: ./system/language/english/front_lang.php */
--- app/libraries/leancloud/samples/send_push.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once 'AV.php';
-$push =new leancloud\AVPush;
-$push->alert = 'Hello from AVOS Cloud';
-$push->channels = array('foo', 'bar');
-$return = $push->send();
-print_r($return);
-?>
